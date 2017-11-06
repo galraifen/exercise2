9527a2d6e0f42fbcb6950688601d97265ef2cd28
galraifen <galraifen15@gmail.com>
2017-11-06 17:52:23 +0200

    EX2

--- app/src/main/java/com/example/magshimim/ex2/MainActivity.java
+++ app/src/main/java/com/example/magshimim/ex2/MainActivity.java
@@ -16,38 +16,40 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
-        Log.i(MY_TAG,"onCreateEvent");
-        Toast.makeText(this,"Welcome user!",Toast.LENGTH_LONG).show();
+        Log.i(MY_TAG,getString(R.string.onCEvent));
+        if(savedInstanceState == null){
+        Toast.makeText(this,getString(R.string.welcome),Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(MY_TAG,"onStartEvent");
+        Log.i(MY_TAG,getString(R.string.onSEvent));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(MY_TAG,"onResumeEvent");
+        Log.i(MY_TAG,getString(R.string.onREvent));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(MY_TAG,"onPauseEvent");
+        Log.i(MY_TAG,getString(R.string.onPEvent));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(MY_TAG,"onStopEvent");
+        Log.i(MY_TAG,getString(R.string.onStopEvent));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(MY_TAG,"onStopEvent");
+        Log.i(MY_TAG,getString(R.string.onDEvent));
     }
 
     public void buttonClicked(View v)
@@ -55,12 +57,29 @@ public class MainActivity extends Activity {
         EditText edName = (EditText)findViewById(R.id.nameText);
         if(edName.getText().length() == 0)
         {
-            Toast.makeText(this,"You must fill the name field before clicking this button",Toast.LENGTH_LONG).show();
+            Toast.makeText(this,getString(R.string.userAlert),Toast.LENGTH_LONG).show();
         }
         else
         {
             this.cnt++;
-            Toast.makeText(this, edName.getText()+": The button was clicked "+cnt+" times",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, edName.getText()+getString(R.string.ansPart1)+cnt+getString(R.string.ansPart2),Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("cnt",cnt);
+        Log.i(MY_TAG,getString(R.string.onSInstance));
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null)
+        {
+            this.cnt = savedInstanceState.getInt("cnt");
+        }
+        Log.i(MY_TAG,getString(R.string.onRInstance));
     }
 }
